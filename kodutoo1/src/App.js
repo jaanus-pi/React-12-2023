9e4a0effe01690a2d9c9a495d9e5459be8600d8e
jaanus-pi <jaanus-soft@proton.me>
2023-12-08 18:27:06 +0200

    Logi sisse 1. osa

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Link, Route, Routes } from "react-router-dom";
+import { useState } from 'react';
 import Avaleht from './pages/Avaleht';
 import Kontakt from './pages/Kontakt';
 import Meist from './pages/Meist';
@@ -8,8 +9,13 @@ import Leht from './pages/Leht';
 import Loader from './pages/Loader';
 
 function App() {
+  const [sisselogitud, muudaSisselogitud] = useState('ei');
+
   return (
     <div className="App">
+      { sisselogitud === 'ei' && <div><button onClick={() => muudaSisselogitud('jah')}>Logi sisse</button> <br /></div>}
+      { sisselogitud === 'jah' && <div><button onClick={() => muudaSisselogitud('ei')}>Logi välja</button> <br /></div>}
+
       <Link to="/">
         <button className="nupp">Avaleht</button>
       </Link>
