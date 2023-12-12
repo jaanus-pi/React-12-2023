949c74c10e1953308449f9f9ccf77b7a9f46961c
jaanus-pi <jaanus-soft@proton.me>
2023-12-12 16:54:45 +0200

    light mode and dark mode

--- kodutoo1/src/pages/Seaded.js
+++ kodutoo1/src/pages/Seaded.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Seaded() {
+  const [teema, muudaTeema] = useState(localStorage.getItem("veebilehe_kujundus"))
+
+  const darkMode = () => {
+    localStorage.setItem("veebilehe_kujundus", "dark_mode")
+    muudaTeema("dark_mode")
+  }
+
+  const lightMode = () => {
+    localStorage.setItem("veebilehe_kujundus", "light_mode")
+    muudaTeema("light_mode")
+  }
+
+  const rainbowMode = () => {
+    localStorage.setItem("veebilehe_kujundus", "rainbow_mode")
+    muudaTeema("rainbow_mode")
+  }
+
   return (
-    <div>Seaded</div>
+    <div>
+      <br />
+      <button onClick={darkMode}>Dark mode</button>
+      <button onClick={lightMode}>Light mode</button>
+      <button onClick={rainbowMode}>Rainbow mode</button>
+      {teema === "dark_mode" && <div>TUME LEHT</div>}
+      {teema === "light_mode" && <div>HELE LEHT</div>}
+      {teema === "rainbow_mode" && <div>VÄRVILINE LEHT</div>}
+    </div>
   )
 }
 
