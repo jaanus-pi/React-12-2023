a94fe8fb84d674851b447bd801eee7ddbdf64fa4
jaanus-pi <jaanus-soft@proton.me>
2023-12-19 16:35:34 +0200

    removing unnecessary imports

--- joogipood/src/pages/HaldaJook.js
+++ joogipood/src/pages/HaldaJook.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import joogidFailist from '../joogid.json'
 
 function HaldaJook() {
--- joogipood/src/pages/LisaJook.js
+++ joogipood/src/pages/LisaJook.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef }from 'react'
+import React, { useRef }from 'react'
 import joogidFailist from '../joogid.json'
 
 function LisaJook() {
