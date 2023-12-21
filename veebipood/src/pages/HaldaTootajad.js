e12c7e1c7c6d231e184df86a7ee5a5880eb5b67c
jaanus-pi <jaanus-soft@proton.me>
2023-12-21 23:24:24 +0200

    halda tootajad page

--- veebipood/src/pages/HaldaTootajad.js
+++ veebipood/src/pages/HaldaTootajad.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import tootajadFailist from '../data/tootajad.json'
 
 function HaldaTootajad() {
+  const [tootajad, uuendaTootajad] = useState(tootajadFailist);
+  const tootajaRef = useRef();
+
+  const lisaTootaja = () => {
+    tootajadFailist.push(tootajaRef.current.value);
+    uuendaTootajad(tootajadFailist.slice());
+  }
+
+  const kustutaTootaja = (index) => {
+    tootajadFailist.splice(index, 1);
+    uuendaTootajad(tootajadFailist.slice());
+  }
+
   return (
-    <div>HaldaTootajad</div>
+    <div>
+      <label>Lisa töötaja: </label>
+      <input ref={tootajaRef} type='text' />
+      <button onClick={lisaTootaja}>Sisesta</button>
+      {tootajad.map((tootaja, index) => 
+        <div key={index}>
+          <span>{tootaja}</span>
+          <button onClick={kustutaTootaja}>x</button>
+        </div>
+      )}
+    </div>
   )
 }
 
--- veebipood/src/pages/Tootajad.js
+++ veebipood/src/pages/Tootajad.js
@@ -16,27 +16,13 @@ function Tootajad() {
     uuendaTootajad(tulem);
   }
 
-  const lisaTootaja = () => {
-    tootajadFailist.push(tootajaRef.current.value);
-    uuendaTootajad(tootajadFailist.slice());
-  }
-
-  const kustutaTootaja = (index) => {
-    tootajadFailist.splice(index, 1);
-    uuendaTootajad(tootajadFailist.slice());
-  }
-
   return (
     <div>
       <button onClick={sorteeri}>Sorteeri</button>
       <button onClick={filtreeri}>Jäta alles kes algab "M" tähega</button> <br />
-      <label>Lisa töötaja: </label>
-      <input ref={tootajaRef} type='text' />
-      <button onClick={lisaTootaja}>Sisesta</button>
       {tootajad.map((tootaja, index) => 
         <div key={index}>
           <span>{tootaja}</span>
-          <button onClick={kustutaTootaja}>x</button>
           <Link to={'/tootaja/' + index}>
             <button>Detailsemalt</button>
           </Link>
