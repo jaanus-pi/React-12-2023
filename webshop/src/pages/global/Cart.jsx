64cde89a0d87d395af990c2cafdf8c56716ae8a9
jaanus-pi <jaanus-soft@proton.me>
2024-01-02 12:59:59 +0200

    dynamic html

--- webshop/src/pages/global/Cart.jsx
+++ webshop/src/pages/global/Cart.jsx
@@ -3,6 +3,7 @@ import cartFromFile from '../../data/cart.json'
 import '../../css/Cart.css';
 import { useState } from 'react';
 import { useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
   const [parcelMachines, setParcelMachines] = useState([]);
@@ -24,22 +25,40 @@ const Cart = () => {
     setCart(cartFromFile.slice());
   }
 
+  const calculateCartTotal = () => {
+    let sum = 0;
+    cart.forEach(product => sum = sum + product.price);
+    return sum;
+  }
+
   return (
     <div>
-      <button onClick={emptyCart}>Empty Cart</button>
+      {cart.length !== 0 ? 
+        <div>
+          Ostukorvis on {cart.length} toode(t)
+          <button onClick={emptyCart}>Empty Cart</button>
+        </div> :
+        <div>
+          <div>Ostukorv on tuhi</div>
+          <Link to='/'>
+            <button>Avalehele</button>
+          </Link>
+        </div>
+      }
       {cart.map((product, index) => 
         <div key={index}>
           <img className='picture' src={product.image} alt='' />
           <span>{product.title} - </span>
           <span>{product.price} EUR</span>
           <button onClick={() => removeFromCart(index)}>x</button>
-          </div>
-          )}
+        </div>
+      )}
       <select>
         {parcelMachines
         .filter(pm => pm.A0_NAME === "EE")
         .map(pm => <option key={pm.NAME}>{pm.NAME}</option>)}
       </select>
+      <div>Kokku: {calculateCartTotal()} - EUR</div>
     </div>
   )
 }
