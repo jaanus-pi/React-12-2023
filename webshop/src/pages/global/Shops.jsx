45c5e0c821cd360f16a78f2cb2d6d9a56eda460d
jaanus-pi <jaanus-soft@proton.me>
2024-01-04 20:16:37 +0200

    buttons from database

--- webshop/src/pages/global/Shops.jsx
+++ webshop/src/pages/global/Shops.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react';
-		
+import { useState, useEffect } from 'react';
+import { Spinner } from 'react-bootstrap'
 import Map from '../../components/Map';
 		
 const Shops = () => {
 		
   const [coordinaates, setCoordinates] = useState({lngLat: [59.4378, 24.7574], zoom: 11});
+  const [shops, setShops] = useState([]);
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_SHOPS_DB_URL)
+      .then(res => res.json())
+      .then(json => {
+        setShops(json);
+      })
+  }, []);
+
+  if (shops.length === 0) {
+    return <Spinner />
+  }
 		
   return (
     <div>
@@ -12,9 +25,14 @@ const Shops = () => {
       <button onClick={() => setCoordinates({lngLat: [59.4378, 24.7574], zoom: 11})}>Kõik Tallinna poed</button>
 
       {/* kuidas homepage sees sai tehtud filterbuttons */}
-      <button onClick={() => setCoordinates({lngLat: [59.4231, 24.7991], zoom: 13})}>Ülemiste</button>
+      {/* <button onClick={() => setCoordinates({lngLat: [59.4231, 24.7991], zoom: 13})}>Ülemiste</button>
       <button onClick={() => setCoordinates({lngLat: [59.4277, 24.7193], zoom: 13})}>Kristiine</button>
-      <button onClick={() => setCoordinates({lngLat: [58.3777, 26.7301], zoom: 13})}>Tasku</button>
+      <button onClick={() => setCoordinates({lngLat: [58.3777, 26.7301], zoom: 13})}>Tasku</button> */}
+      <div>
+        {shops.map(shop => 
+          <button key={shop.name} onClick={() => setCoordinates({lngLat: [shop.longitude, shop.latitude], zoom: 13})}>{shop.name}</button>
+        )}
+      </div>
 
       <Map mapCoordinaates={coordinaates}  />
     </div>
