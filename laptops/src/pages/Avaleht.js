0d17f620e7cf515eac4c94e3c1cb2def6746c4e9
jaanus-pi <jaanus-soft@proton.me>
2024-01-05 15:22:47 +0200

    Add pc's to localStorage

--- laptops/src/pages/Avaleht.js
+++ laptops/src/pages/Avaleht.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 function Avaleht() {
+  const computers = JSON.parse(localStorage.getItem("laptops")) || [];
+
   return (<div className='avaleht-text'>
     <div>Tere</div> <br /><br />
     <div>Siin lehel saad sülearvuteid vaadata ja lisada</div>
+    { computers.map(oneComputer =>
+      <div>
+        <div>{oneComputer.mark}</div>
+        <div>{oneComputer.mudel}</div>
+        <div>{oneComputer.maksumus}</div>
+        <button>Nuppp</button>
+      </div>
+    ) }
   </div>
   )
 }
--- laptops/src/pages/LisaArvuti.js
+++ laptops/src/pages/LisaArvuti.js
@@ -1,23 +1,36 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 function LisaArvuti() {
   const [message, setMessage] = useState('Lisa arvuti!');
   const [n2itaNuppu, uuendaN2itaNuppu] = useState(true);
+  const markRef = useRef();
+  const mudelRef = useRef();
+  const maksumusRef = useRef();
   
   const addProduct = () => {
     setMessage('Arvuti lisatud!');
     uuendaN2itaNuppu(false);
+
+    const newComputer = {
+      "mark": markRef.current.value,
+      "mudel": mudelRef.current.value,
+      "maksumus": maksumusRef.current.value
+    }
+
+    const computers = JSON.parse(localStorage.getItem("laptops")) || [];
+    computers.push(newComputer);
+    localStorage.setItem("laptops", JSON.stringify(computers));
   }
 
   return (
     <div>
       <div>Sõnum: {message}</div>
       <label>Mark</label> <br />
-      <input type='text' /> <br />
+      <input ref={markRef} type='text' /> <br />
       <label>Mudel</label> <br />
-      <input type='text' /> <br />
+      <input ref={mudelRef} type='text' /> <br />
       <label>Maksumus</label> <br />
-      <input type='number' /> <br />
+      <input ref={maksumusRef} type='number' /> <br />
       { n2itaNuppu === true && <button onClick={() => addProduct()}>Sisesta</button>}
     </div>
   )
