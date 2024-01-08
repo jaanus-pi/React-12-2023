093a225ff4eed50fa02784673fe7e0ac4719b4b8
jaanus-pi <jaanus-soft@proton.me>
2024-01-08 13:23:36 +0200

    yl32, tegelaste valimine

--- tegelased/src/pages/Avaleht.js
+++ tegelased/src/pages/Avaleht.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react'
 
 function Avaleht() {
   const [kuva, uuendaKuva] = useState("");
+  const valitudTegelasedLS = JSON.parse(localStorage.getItem("valitudTegelased")) || [];
 
   const tegelased = [
-    {"eesnimi": "Mickey", "perenimi": "Mouse", "elukoht": "Disneyland"},
-    {"eesnimi": "Minnie", "perenimi": "Mouse", "elukoht": "Disneyland"},
-    {"eesnimi": "Winnie", "perenimi": "Pooh", "elukoht": "Hundred Acre Wood"},
-    {"eesnimi": "Roo", "perenimi": "Kangaroo", "elukoht": "Hundred Acre Wood"},
-    {"eesnimi": "Scooby", "perenimi": "Doo", "elukoht": "Crystal Cove"}
+    {eesnimi: "Mickey", perenimi: "Mouse", elukoht: "Disneyland", vanus: 120},
+    {eesnimi: "Minnie", perenimi: "Mouse", elukoht: "Disneyland", vanus: 1123},
+    {eesnimi: "Winnie", perenimi: "Pooh", elukoht: "Hundred Acre Wood", vanus: 122},
+    {eesnimi: "Roo", perenimi: "Kangaroo", elukoht: "Hundred Acre Wood", vanus: 12},
+    {eesnimi: "Scooby", perenimi: "Doo", elukoht: "Crystal Cove", vanus: 1220}
   ];
 
   const kuvaNimi = (tegelane) => {
     uuendaKuva(tegelane.eesnimi)
+  }
+
+  const valiTegelane = (klikutudTegelane) => {
+    valitudTegelasedLS.push(klikutudTegelane);
+    localStorage.setItem("valitudTegelased", JSON.stringify(valitudTegelasedLS));
   }
   
   return (
@@ -23,6 +29,8 @@ function Avaleht() {
           <div onClick={() => kuvaNimi(tegelane)}>{tegelane.eesnimi}</div>
           <div>{tegelane.perenimi}</div>
           <div>{tegelane.elukoht}</div>
+          <div>{tegelane.vanus}</div>
+          <button onClick={() => valiTegelane(tegelane)}>Vali tegelane</button>
         </div>
         )}
     </div>
--- tegelased/src/pages/ValitudTegelased.js
+++ tegelased/src/pages/ValitudTegelased.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ValitudTegelased() {
+  const [valitudTegelased, uuendaValitudTegelased] = useState(JSON.parse(localStorage.getItem("valitudTegelased")) || []);
+  
+  const eemaldaYksTegelane = (index) => {
+    valitudTegelased.splice(index, 1);
+    uuendaValitudTegelased(valitudTegelased.slice());
+    localStorage.setItem("valitudTegelased", JSON.stringify(valitudTegelased));
+  }
+
+  const eemaldaKoik = () => {
+    uuendaValitudTegelased([]);
+    localStorage.setItem("valitudTegelased", JSON.stringify([]));
+  }
+
   return (
-    <div>ValitudTegelased</div>
+    <div>
+      {valitudTegelased.length !== 0 ?
+      <div>
+        <div>Kokku on valitud {valitudTegelased.length} tegelast.</div>
+        <button onClick={eemaldaKoik}>Eemalda kõik</button>
+      </div> :
+      <div>
+        <div>Ühtegi tegelast pole valitud.</div>
+      </div>}
+      <div>{valitudTegelased.map((tegelane, index) =>
+        <div key={index}>
+          <div>{tegelane.eesnimi}</div>
+          <div>{tegelane.perenimi}</div>
+          <div>{tegelane.elukoht}</div>
+          <div>{tegelane.vanus}</div>
+          <button onClick={() => eemaldaYksTegelane(index)}>X</button>
+        </div>
+      )}</div>
+    </div>
   )
 }
 
