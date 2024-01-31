940a777bfcd493f57ef2b66b76ef74c7acc455b8
jaanus-pi <jaanus-soft@proton.me>
2024-01-31 17:36:30 +0200

    full posts

--- kidraleht/src/pages/posts/CountryRoads.jsx
+++ kidraleht/src/pages/posts/CountryRoads.jsx
@@ -19,9 +19,27 @@ const CountryRoads = () => {
         <div className="singlePostInfo">
           <span className="singlePostAuthor">Autor: name</span>
         </div>
-        <p className="postContent">
-          Lorem ipsum
-        </p>
+        <div className="postContent">
+          {post.description}
+          <hr />
+          <div>
+            <p>
+              Proovi mängida igat akordi 4 tõmmet. Kui tunned ennast mugavalt, proovi kaasa mängida YouTubes võttes kiirust alla, nt 0.5x kiirusel. Kaasa mängimiseks seda lugu oleks vaja kasutada <b>capot</b>, mille paned <b>teise vahesse</b>. Akordide vormid jäävad kõik samaks, kuid nüüd hakkad vahesi lugema alates capost. Mõned näited on all piltidel.
+              Rütmikaks saad kasutada igalpool näiteks 4 tõmmet, välja arvatud salmi ridade lõpus, kus märgitud teisiti 2t (kaks tõmmet).
+            </p>
+            <img src={require("../../assets/posts/country-roads/tabs.png")} alt='tabs' />
+          </div>
+          <div>
+            <p>Akordivormid:</p>
+            <img src={require("../../assets/posts/country-roads/C.png")} alt='C' />
+            <img src={require("../../assets/posts/country-roads/Em.png")} alt='Em' />
+            <img src={require("../../assets/posts/country-roads/D.png")} alt='D' />
+            <img src={require("../../assets/posts/country-roads/G.png")} alt='G' />
+            <p>Capo 2 vahes: esimesel pildil on G vorm ja teisel D vorm:</p>
+            <img src={require("../../assets/posts/country-roads/capoG.png")} alt='Capo G' />
+            <img src={require("../../assets/posts/country-roads/capoD.png")} alt='Capo D' />
+          </div>
+        </div>
       </div>
     </div>
       <Sidebar />
--- kidraleht/src/pages/posts/KaugesKylas.jsx
+++ kidraleht/src/pages/posts/KaugesKylas.jsx
@@ -19,9 +19,16 @@ const KaugesKylas = () => {
         <div className="singlePostInfo">
           <span className="singlePostAuthor">Autor: name</span>
         </div>
-        <p className="postContent">
-          Lorem ipsum
-        </p>
+        <div className="postContent">
+          {post.description}
+          <hr />
+          <div>
+            <p>Selle loo mängimiseks on kaks võimalust. Esimesena on akordid kasutades <b>capot 2. vahes</b>. Siin on uus akord F, mille kõige levinumat vormi nimetatakse barre akordiks (bar chord), selle võime hetkel vahele jätta.</p>
+            <img src={require("../../assets/posts/kauges-kylas/tabs-capo-2.png")} alt='Tabs capo 2' />
+            <p>Teine viis seda lugu mängida on <b>ilma capota</b>, kasutades neid akorde. Siin on aga meie jaoks uus Bm, mille kõige levinum vorm on samuti barre akord. Seega võime ka siin hetkel selle lihtsalt vahele jätta.</p>
+            <img src={require("../../assets/posts/kauges-kylas/tabs-no-capo.png")} alt='Tabs no capo' />
+          </div>
+        </div>
       </div>
     </div>
       <Sidebar />
